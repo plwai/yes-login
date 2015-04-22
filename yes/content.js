7556a62326da8145ff7bb957274553b88413e9dc
plwai <pailee.wai@gmail.com>
2015-04-22 23:29:20 +0900

    First version of yes login extension.

---
+++ yes/content.js
@@ -0,0 +1,6 @@
+document.getElementById("light1").style.display = 'none';
+document.getElementById("fade").style.display = 'none';
+document.getElementById("yesID").value = 'UTM Student';
+document.getElementById("checker").checked  = true;
+var elem = document.getElementsByClassName('blueButtonLG floatLeft');
+elem[0].click();
